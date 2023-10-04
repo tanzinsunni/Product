9e1347729356bfa1238c0f9855e33b83965d04f0
tanzinsunni <tanzinsunni@gmail.com>
2023-10-04 11:53:34 +0600

    fourth commit

---
+++ Product.php
@@ -0,0 +1,29 @@
+<?php
+class Product {
+    private $id;
+    private $name;
+    private $price;
+    
+    public function  __construct($id, $name, $price) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getFormattedPrice() {
+        return '$' . number_format($this->price, 2);
+    }
+
+    public function showDetails() {
+        echo "Product Details:";
+        echo"<br>";
+        echo "- ID: " . $this->id . "<br>";
+        echo "- Name: " . $this->name . "<br>";
+        echo "- Price: " . $this->getFormattedPrice() . "<br>";
+    }
+}
+
+// Test the Product class
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+?>
